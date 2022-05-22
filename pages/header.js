1d555bec1b22091f4bdb206b21d6e1429d72fe97
Mateo Annikian <mattannikian@gmail.com>
2022-05-22 02:54:15 -0300

    DALE WACHO

--- pages/header.js
+++ pages/header.js
@@ -3,10 +3,12 @@ window.addEventListener('scroll', function () {
     var anchor = document.querySelector('a')
     var imageNone = document.querySelector('.logo');
     var scrollDos = document.querySelector('.logo-2');
+    var toggle = document.querySelector('.hamburger');
     header.classList.toggle('abajo', window.scrollY > 20);
     anchor.classList.toggle('abajo', window.scrollY > 20);
     imageNone.classList.toggle('abajo', window.scrollY > 20);
-    scrollDos.classList.toggle('abajo', window.scrolly > 20);
+    scrollDos.classList.toggle('abajo', window.scrollY > 20);
+    toggle.classList.toggle('abajo', window.scrollY > 20)
     
   });
 
