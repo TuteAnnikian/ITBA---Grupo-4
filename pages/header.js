1bf893a33a2b3ab2fbb44d7fe1436bf874518196
Mateo Annikian <mattannikian@gmail.com>
2022-05-22 02:07:02 -0300

    realice cambios

---
+++ pages/header.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    var anchor = document.querySelector('a')
+    var imageNone = document.querySelector('.logo');
+    var scrollDos = document.querySelector('.logo-2');
+    header.classList.toggle('abajo', window.scrollY > 20);
+    anchor.classList.toggle('abajo', window.scrollY > 20);
+    imageNone.classList.toggle('abajo', window.scrollY > 20);
+    scrollDos.classList.toggle('abajo', window.scrolly > 20);
+    
+  });
+
+ 
+  const hamburgerMenu = document.querySelector('.hamburger');
+  const menuIsActive = () => {
+    hamburgerMenu.classList.toggle('active');
+  };
+  hamburgerMenu.addEventListener('click', menuIsActive);
