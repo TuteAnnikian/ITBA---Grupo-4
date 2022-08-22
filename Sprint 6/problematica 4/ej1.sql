bb05d2ac9cf34abf0c9231c6ec32fa4f49a187fe
Ms2502 <105183566+Ms2502@users.noreply.github.com>
2022-08-22 10:34:15 -0300

    sprint6 actualizado

--- Sprint 6/problematica 4/ej1.sql
+++ Sprint 6/problematica 4/ej1.sql
@@ -1,1 +1,4 @@
-SELECT sucursal.branch_name AS "Nombre_de_la_sucursal", COUNT (customer_id) AS "Total_por_sucursal" FROM cliente JOIN sucursal ON cliente.branch_id = sucursal.branch_id GROUP BY sucursal.branch_name ORDER BY Total_por_sucursal DESC
+SELECT sucursal.branch_name AS "Nombre_de_la_sucursal", COUNT (customer_id) AS "Total_por_sucursal" 
+FROM cliente JOIN sucursal ON cliente.branch_id = sucursal.branch_id 
+GROUP BY sucursal.branch_name 
+ORDER BY Total_por_sucursal DESC
--- Sprint 6/problematica 4/ej5.sql
+++ Sprint 6/problematica 4/ej5.sql
@@ -1,4 +1,5 @@
 CREATE TABLE "auditoria_cuenta" (
+CREATE TABLE IF NOT EXISTS "auditoria_cuenta"(
 "change_id" INTEGER NOT NULL,
 "old_id" INTEGER, 
 "new_id" INTEGER,
@@ -11,13 +12,17 @@ CREATE TABLE "auditoria_cuenta" (
 "user_action" TEXT,
 "created_at" TEXT,
 PRIMARY KEY ("change_id" AUTOINCREMENT)
-)
+);
 
-CREATE TRIGGER auditoria
+CREATE TRIGGER IF NOT EXISTS auditoria 
 AFTER UPDATE ON cuenta
 BEGIN
 INSERT INTO auditoria_cuenta (old_id, new_id, old_balance, new_balance, old_iban, new_iban, old_type, new_type, user_action, created_at)
-VALUES (old.account_id), (new.account_id), (old.balance), (new.balance), (old.iban), (new.iban), (old.tipo_cuenta_id), (new.tipo_cuenta_id), ("Actualiza datos"), (DATETIME(now));
-END
+VALUES (old.account_id, new.account_id, old.balance, new.balance, old.iban, new.iban, old.tipo_cuenta_id, new.tipo_cuenta_id, "Actualiza datos", DATETIME("now"));
+END;
 
-
+UPDATE cuenta SET balance = balance - 100 WHERE account_id = 10;
+UPDATE cuenta SET balance = balance - 100 WHERE account_id = 11;
+UPDATE cuenta SET balance = balance - 100 WHERE account_id = 12;
+UPDATE cuenta SET balance = balance - 100 WHERE account_id = 13;
+UPDATE cuenta SET balance = balance - 100 WHERE account_id = 14;
